da9df4b4c538a181d7f14ac62ef49554de01afee
BogdanLubiany <bogdanlubya@gmail.com>
2023-08-31 12:58:05 +0300

    first commit

---
+++ scriptsmenu.js
@@ -0,0 +1,11 @@
+const menuToggle = document.getElementById('mobile-menu-toggle');
+
+const navLinks = document.getElementById('nav-links');
+
+
+
+menuToggle.addEventListener('click', () => {
+
+    navLinks.classList.toggle('active');
+
+});
